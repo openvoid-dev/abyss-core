1665880fc40d20e5db8402d0fa22643f73a0ca76
AntonioObra <antonio@obradovic.dev>
2024-10-25 19:29:12 +0200

    added: tailwindcss with its cli without npm or nodejs

--- src/Abyss/Whisper/Serve.php
+++ src/Abyss/Whisper/Serve.php
@@ -6,11 +6,26 @@ use Abyss\Core\Application;
 
 class Serve
 {
-    public function handle($host = "0.0.0.0", $port = "8383")
+    /**
+     * Start the Abyss server and tailwind server
+     *
+     * @param string $host
+     * @param string $port
+     * @return void
+     **/
+    public function handle($host = "0.0.0.0", $port = "8383"): void
     {
+        echo "Abyss development server started: http://{$host}:{$port}\n";
+        echo "Started tailwindcss watcher in the background.\n";
+
         $public_dir = Application::get_base_path("/public");
 
-        echo "Abyss development server started: http://{$host}:{$port}\n";
+        // * Run Tailwind watcher in the background
+        $tailwindCommand =
+            "tailwindcss -i ./app/resources/css/main.css -o ./public/css/main.css --watch > /dev/null 2>&1 &";
+        passthru($tailwindCommand);
+
+        // * Run the PHP built-in server
         passthru("php -S {$host}:{$port} -t {$public_dir}");
     }
 }
